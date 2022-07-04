49e83fcd4da2a704ca9323f475dfbf77c16d2037
jolantalei <jolanta.leigute@gmail.com>
2022-07-04 11:20:52 +0300

    first commit

---
+++ demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
